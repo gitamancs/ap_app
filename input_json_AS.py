9cdb5c085b2de131f6e739a1635b5b3101e8de5c
Aman khan <132432903+gitamancs@users.noreply.github.com>
2025-07-11 15:19:57 +0530

    Create input_json_AS.py

---
+++ input_json_AS.py
@@ -0,0 +1,92 @@
+AS_input_json = {
+    "task": "map_to_department",
+    "pincode": "500081",
+    "departments": [
+      "Gynecology & Obstetrics",
+      "Endocrinology"
+    ],
+    "symptom": "irregular periods",
+    "selected_date": "2025-06-06",
+    "age": 20,
+    "gender": "F",
+    "followup_answers": [
+      {
+        "question": "How long have you been experiencing irregular periods, and have they always been irregular or was there a change at some point?",
+        "answer": "2 months sudden change"
+      },
+      {
+        "question": "Are your periods accompanied by other symptoms such as heavy bleeding, severe cramps, or excessive hair growth?",
+        "answer": "severe cramps"
+      }
+    ],
+    "summary": "Irregular periods for 2 months following a sudden change. Accompanied by severe cramps. No significant weight changes, appetite changes, or energy level changes. Not using any birth control or hormonal medication.",
+    "branches": [
+      {
+        "Pincode": 500032,
+        "Branch": "Gachibowli",
+        "distance": 3.248389544886172
+      },
+      {
+        "Pincode": 500072,
+        "Branch": "Kukatpally",
+        "distance": 5.12907950722268
+      }
+    ],
+    "final_dr_list": [
+      {
+        "Doctor_ID": "DOC017",
+        "Available_Dates": [
+          {
+            "Available_Date": "2025-06-03",
+            "Appointment_Load": 45,
+            "Time_Slot": "9:00 AM - 12:00 PM"
+          }
+        ]
+      },
+      {
+        "Doctor_ID": "DOC020",
+        "Available_Dates": [
+          {
+            "Available_Date": "2025-06-03",
+            "Appointment_Load": 47,
+            "Time_Slot": "4:30 PM - 7:30 PM"
+          }
+        ]
+      },
+      {
+        "Doctor_ID": "DOC053",
+        "Available_Dates": [
+          {
+            "Available_Date": "2025-06-03",
+            "Appointment_Load": 46,
+            "Time_Slot": "9:30 AM - 12:30 PM"
+          }
+        ]
+      },
+      {
+        "Doctor_ID": "DOC170",
+        "Available_Dates": [
+          {
+            "Available_Date": "2025-06-03",
+            "Appointment_Load": 37,
+            "Time_Slot": "1:00 PM - 4:00 PM"
+          }
+        ]
+      }
+    ],
+    "grouped_text": "Doctor: Dr. G. Vindhya (ID: DOC170) - Branches: Gachibowli - Department: Gynecology & Obstetrics\n  - Menstrual irregularities: 1\n  - Pelvic pain: 1\n\nDoctor: Dr. K Aswini (ID: DOC017) - Branches: Kukatpally - Department: Gynecology & Obstetrics\n - Menstrual irregularities: 1\n\nDoctor: Dr. Y. Sravani (ID: DOC020) - Branches: Kukatpally - Department: Gynecology & Obstetrics\n - Menstrual irregularities: 2\n - Pelvic pain: 1\n\n",
+    "raw_text": "1. Dr. Y. Sravani (ID: DOC020) - Branch: Kukatpally - Department: Gynecology & Obstetrics\n   Treated 2 cases of menstrual irregularities and 1 cases of pelvic pain\n   Located in the user's nearest branch and experienced in treating relevant conditions.\n\n2. Dr. K Aswini (ID: DOC017) - Branch: Kukatpally - Department: Gynecology & Obstetrics\n   Treated 1 cases of menstrual irregularities\n   Located in the user's nearest branch and experienced in treating relevant conditions.\n\n3. Dr. G. Vindhya (ID: DOC170) - Branch: Gachibowli - Department: Gynecology & Obstetrics\n   Treated 1 cases of menstrual irregularities\n   Experienced in treating relevant conditions, though not in the nearest branch with the most relevant count.",
+    "doctor_ids_ordered": [
+      "DOC020",
+      "DOC017",
+      "DOC170"
+    ],
+    "recommended_doctors": "1. Dr. Y. Sravani (ID: DOC020) - Branch: Kukatpally - Department: Gynecology & Obstetrics\n   Available Date: 2025-06-03\n   Available Time Slot: 4:30 PM - 7:30 PM\n   Treated 3 cases of menstrual irregularities and 3 cases of pelvic pain\n   Located in the user's nearest branch and experienced in treating relevant conditions.\n\n2. Dr. G. Vindhya (ID: DOC170) - Branch: Gachibowli - Department: Gynecology & Obstetrics\n   Available Date: 2025-06-03\n   Available Time Slot: 1:00 PM - 4:00 PM\n   Treated 2 cases of menstrual irregularities\n   Experienced in treating relevant conditions, though not in the nearest branch with the most relevant count.\n\n3. Dr. K Aswini (ID: DOC017) - Branch: Kukatpally - Department: Gynecology & Obstetrics\n   Available Date: 2025-06-03\n   Available Time Slot: 9:00 AM - 12:00 PM\n   Treated 2 cases of menstrual irregularities and 2 cases of endometriosis treatment\n   Located in the user's nearest branch and experienced in treating relevant conditions.",
+    "appointment_data": {
+      "Email": ""
+    },
+    "appointment_id": "APT123",
+    "return_all": False
+  }
+
+ 
